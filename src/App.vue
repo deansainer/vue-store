bd3a1dabbe9ae734e029857cf76d6ecb367795e3
deansainer <determind2k@gmail.com>
2025-02-26 19:40:50 +0100

    header created

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    
+  <div class="text-2xl w-4/5 m-auto bg-white min-h-screen p-4 rounded-xl mt-20 shadow-xl">
+    <header class="flex justify-between border-b p-4 border-gray-300">
+      <div class="flex items-center">
+        <img class="w-14" alt="logo" src="/logo.png">
+        <div class="ml-4">
+          <h1 class="text-xl font-bold uppercase">Warsaw sneaker store</h1>
+          <p class="text-slate-500 text-base">Best sneakers in Warsaw</p>
+        </div>
+      </div>
+
+
+    <ul class="flex items-center">
+      <li class="ml-8 flex items-center">
+        <img src="/cart.svg">
+        <p class="text-sm ml-2">38.99 pln</p>
+      </li>
+
+      <li class="ml-4 flex items-center">
+        <img src="/heart.svg">
+        <p class="text-sm ml-2">Favorites</p>
+      </li>
+
+      <li class="ml-4 flex items-center">
+        <img src="/profile.svg">
+        <p class="text-sm ml-2">Profile</p>
+      </li>
+    </ul>
+
+    </header>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
